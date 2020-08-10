7dd80d2a08769530fc6fc6f074caa796d0f767d0
Vinnie Malvi <vinod.malvi@yahoo.co.uk>
2020-08-10 23:05:52 +0100

    Added few more tests

--- StringCalculator.Core/StringCalculator.cs
+++ StringCalculator.Core/StringCalculator.cs
@@ -11,11 +11,28 @@ namespace StringCalculator.Core
 
         public int Add(string numbers)
         {
+            var delimiters = ",\n";
+
             if (string.IsNullOrEmpty(numbers))
                 return 0;
-            if (numbers.Contains(","))
-                return numbers.Split(',').Select(x=>int.Parse(x)).Sum();
-            return 1;
+
+            if (numbers.Contains("//"))
+            {
+                delimiters += numbers[2];
+                numbers = numbers.Substring(4, numbers.Length - 4);
+            }
+
+            var nums = numbers.Split(delimiters.ToCharArray());
+
+            if (nums.Any(x => string.IsNullOrEmpty(x)))
+                throw new ArgumentException("Consecutive delimiters not allowed.");
+
+            var ints = nums.Select(x => int.Parse(x));
+
+            if (ints.Any(x => x < 0))
+                throw new ArgumentOutOfRangeException();
+
+            return nums.Select(x=>int.Parse(x)).Sum();
         }
     }
 }
--- StringCalculator.Tests/StringCalculatorTests.cs
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace StringCalculator.Tests
@@ -50,12 +51,36 @@ namespace StringCalculator.Tests
         }
 
         [TestMethod]
-        public void Given_New_Line_As_A_Delimeter_Then_Return_Sum_Of_All_Numbers()
+        public void Given_When_New_Line_As_A_Delimiter_Then_Return_Sum_Of_All_Numbers()
         {
             var expected = 6;
             var actual = strCalc.Add("1\n2,3");
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Given_When_Consecutive_Delimiters_Then_Throw_Exception()
+        {
+            var expected = "Consecutive delimiters not allowed.";
+            var exception = Assert.ThrowsException<ArgumentException>(() => strCalc.Add(",,"));
+
+            Assert.AreEqual(expected, exception.Message);
+        }
+
+        [TestMethod]
+        public void Given_When_A_Custom_Delimiter_Then_Return_Sum_Of_All_Numbers()
+        {
+            var expected = 3;
+            var actual = strCalc.Add("//;\n1;2");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Given_When_A_Negative_Number_Then_Throw_Exception()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => strCalc.Add("-1, 2"));
         }
     }
 }
