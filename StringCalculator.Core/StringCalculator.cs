49bee9f557a82fb078bf581e660f9760d24fa1fb
D0tNetGeek <vmalvi2003@yahoo.com>
2020-08-12 02:05:14 +0100

    Refactored code

--- StringCalculator.Core/StringCalculator.cs
+++ StringCalculator.Core/StringCalculator.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Linq;
+using System.Text;
 
 namespace StringCalculator.Core
 {
@@ -7,46 +8,133 @@ namespace StringCalculator.Core
     {
         public int Add(string numbers)
         {
-            var delimiters = ",\n";
+            //Replace all '\n' with ',' and check if new delimiter is requested
+            //Throws exceptiion if requested delimiter is not allowed
+            numbers = HandleString(numbers);
 
-            if (IsEmptyString(numbers))
-                return HandleEmptyString();
+            //Throws exception if "-" is present in the string
+            CheckNegativeNumbers(numbers);
 
-            if (numbers.Contains("//"))
-            {
-                delimiters += numbers[2];
-                numbers = numbers.Substring(4, numbers.Length - 4);
-            }
+            //Removes all numbers larger than 1000 from the string
+            numbers = RemoveLargeNumbers(numbers);
 
-            var nums = numbers.Split(delimiters.ToCharArray());
-
-            if (nums.Any(x => string.IsNullOrEmpty(x)))
-                throw new ArgumentException("Consecutive delimiters not allowed.");
-
-            var ints = nums.Select(x => int.Parse(x));
-            var negNums = ints.Where(x => x < 0);
-
-            if (negNums.Count() > 0)
-            {
-                var message = $"Negatives not allowed - {string.Join(",",negNums.Select(x=>x.ToString()).ToArray())}";
-
-                throw new ArgumentOutOfRangeException(message);
-            }
-
-            if (ints.Any(x => x < 0))
-                throw new ArgumentOutOfRangeException();
-
-            return nums.Select(x=>int.Parse(x)).Sum();
+            return Sum(SplitString(numbers));
         }
 
-        private static int HandleEmptyString()
+        private static string HandleString(string numbers)
         {
-            return 0;
+            //Replace all newlines with the default delimiter to simplify expression handling
+            numbers = numbers.Replace("\n", ",");
+
+            if (numbers.StartsWith("//["))
+            {
+                //Extract the delimiter string between the brackets
+                string delimiter = numbers.Substring(3, numbers.IndexOf("]"));
+
+                //Check for any illegal delimiter
+                if (delimiter.Contains("-"))
+                    throw new ArgumentException($"Illegal delimiter: {delimiter}");
+
+                //Throw away the "//[delimiter]\n" part
+                numbers = numbers.Substring(5 + delimiter.Length);
+
+                //Replace all instances of the new delimiter with default one
+                numbers = numbers.Replace(delimiter, ",");
+            }
+
+            if (numbers.StartsWith("//"))
+            {
+                //Check if string contains int
+                if (IsStringContainsInt(numbers) == "0") return "0";
+
+                //Extract new delimiter
+                string delimiter = numbers.Substring(2, 3);
+
+                //Check for the illegal delimiter
+                if(delimiter.Contains("-"))
+                    throw new ArgumentException($"Illegal delimiter: {delimiter}");
+
+                //Throw away the "//[delimiter]\n" part
+                numbers = numbers.Substring(4);
+
+                //Replace all instances of the new delimiter with default one
+                numbers = numbers.Replace(delimiter, ",");
+            }
+
+            return numbers;
         }
 
-        private static bool IsEmptyString(string numbers)
+        private static void CheckNegativeNumbers(string numbers)
         {
-            return numbers.Length == 0;
+            string[] negNums = SplitString(numbers);
+
+            string negatives = "";
+
+            foreach(var negNum in negNums)
+            {
+                if (negNum.Contains("-"))
+                {
+                    negatives += negNum + ",";
+                }
+            }
+
+            if (negatives.Count() > 0)
+            {
+                throw new ArgumentException($"Negatives not allowed: {negatives.TrimEnd(',')}");
+            }
+        }
+
+        private static string RemoveLargeNumbers(string numbers)
+        {
+            StringBuilder fixedString = new StringBuilder();
+
+            string[] nums = SplitString(numbers);
+
+            foreach(string num in nums)
+            {
+                if(ToInt(num) <= 1000)
+                {
+                    fixedString.Append(num + ",");
+                }
+            }
+
+            return fixedString.ToString();
+        }
+
+        private static string[] SplitString(string numbers)
+        {
+            string[] nums = numbers.Split(new char[] { ',', ';', '%', '*' });
+
+            return nums;
+        }
+
+        private static int ToInt(string number)
+        {
+            return int.Parse(IsNotNullString(number));
+        }
+
+        private static int Sum(string[] numbers)
+        {
+            int sum = 0;
+
+            foreach(string number in numbers)
+            {
+                sum += ToInt(number);
+            }
+
+            return sum;
+        }
+
+        private static string IsNotNullString(string number)
+        {
+            return string.IsNullOrEmpty(number.ToString()) ? "0" : number;
+        }
+
+        private static string IsStringContainsInt(string numbers)
+        {
+            var result = new String(numbers.Where(Char.IsDigit).ToArray());
+
+            return string.IsNullOrEmpty(result) ? "0" : result;
         }
     }
 }
